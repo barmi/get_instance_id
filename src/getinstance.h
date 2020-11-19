282d2cabb895fc2e2b3c2bec64560f5aaa75ee90
skshin <skshin@gmail.com>
2020-11-19 18:52:22 +0900

    init commit source (not compile yet)

---
+++ src/getinstance.h
@@ -0,0 +1,8 @@
+//
+// Created by skshin on 2020/11/19.
+//
+
+#ifndef SIMPLE_WGET_C_SRC_GETINSTANCE_H_
+#define SIMPLE_WGET_C_SRC_GETINSTANCE_H_
+
+#endif //SIMPLE_WGET_C_SRC_GETINSTANCE_H_
---
+++ src/main.c
@@ -0,0 +1,11 @@
+//
+// Created by skshin on 2020/11/19.
+//
+// ref: https://stackoverflow.com/questions/11208299/how-to-make-an-http-get-request-in-c-without-libcurl
+
+#include "getinstance.h"
+
+int main()
+{
+  return 0;
+}
