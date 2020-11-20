ae2a1f6eab7346fc327b68e51f8595a379aaaac0
skshin <skshin@gmail.com>
2020-11-20 12:59:49 +0900

    getinstance.c: fixed check the return value of get_instance_proc()

---
+++ cmake-build-release/CMakeFiles/getinstance.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/skshin/git/simple_wget_c/src/getinstance.c" "/Users/skshin/git/simple_wget_c/cmake-build-release/CMakeFiles/getinstance.dir/src/getinstance.c.o"
+  "/Users/skshin/git/simple_wget_c/src/main.c" "/Users/skshin/git/simple_wget_c/cmake-build-release/CMakeFiles/getinstance.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
