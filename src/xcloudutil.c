9ca5f5cf2162ed07ce3c6cbef26ad159ad0b01dd
skshin <skshin@gmail.com>
2022-01-27 16:52:07 +0900

    connection timeout 처리

--- src/xcloudutil.c
+++ src/xcloudutil.c
@@ -18,12 +18,68 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <errno.h>
+#include <err.h>
 
 #define MAX_REQUEST_LEN 1024
 
 #ifdef __APPLE__
 #define strcasestr strstr
 #endif
+
+int connect_nonb(int sockfd, const struct sockaddr *saptr, int salen, int nsec)
+{
+  int             flags, n, error;
+  socklen_t       len;
+  fd_set          rset, wset;
+  struct timeval  tval;
+
+  flags = fcntl(sockfd, F_GETFL, 0);
+  fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
+
+  error = 0;
+  if ( (n = connect(sockfd, (struct sockaddr *) saptr, salen)) < 0)
+    if (errno != EINPROGRESS)
+      return(-1);
+
+  /* Do whatever we want while the connect is taking place. */
+
+  if (n == 0)
+    goto done;  /* connect completed immediately */
+
+  FD_ZERO(&rset);
+  FD_SET(sockfd, &rset);
+  wset = rset;
+  tval.tv_sec = nsec;
+  tval.tv_usec = 0;
+
+  if ( (n = select(sockfd+1, &rset, &wset, NULL,
+                   nsec ? &tval : NULL)) == 0) {
+    close(sockfd);      /* timeout */
+    errno = ETIMEDOUT;
+    return(-1);
+  }
+
+  if (FD_ISSET(sockfd, &rset) || FD_ISSET(sockfd, &wset)) {
+    len = sizeof(error);
+    if (getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &error, &len) < 0)
+      return(-1);         /* Solaris pending error */
+  } else {
+    return (-1);
+//    err_quit("select error: sockfd not set");
+  }
+
+  done:
+  fcntl(sockfd, F_SETFL, flags);  /* restore file status flags */
+  if (error) {
+    close(sockfd);      /* just in case */
+    errno = error;
+    return(-1);
+  }
+  return(0);
+}
 
 static int get_instance_proc(char *hostname, char *request, char *id)
 {
@@ -67,7 +123,7 @@ static int get_instance_proc(char *hostname, char *request, char *id)
   sockaddr_in.sin_port = htons(server_port);
 
   /* Actually connect. */
-  if (connect(socket_file_descriptor, (struct sockaddr*)&sockaddr_in, sizeof(sockaddr_in)) == -1) {
+  if (connect_nonb(socket_file_descriptor, (struct sockaddr*)&sockaddr_in, sizeof(sockaddr_in), 1) == -1) {
     return GI_ERROR_CONNECT;
   }
 
